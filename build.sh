c84356605f9521dd15bd9d76ac075044a6d78cdc
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-03-09 14:39:32 -0500

    Add icons, build .zip

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo "Make sure you call this from the source directory"
 
 BUILD_DIR="release-builds"
 
-electron-packager . --overwrite --platform=darwin --arch=x64  --prune=true --out=$BUILD_DIR
+electron-packager . --overwrite --platform=darwin --arch=x64  --prune=true --out=$BUILD_DIR --icon=icons.icns
 
 
 #cd into the build directory, otherwise tar will misbehave
@@ -17,11 +17,13 @@ cd "Raspberry Pi Remote Desktop-darwin-x64"
 #Therefore, we should ask before creating the .xz file 
 
 
-read -n 1 -p "Should a compressed archive be created? (y/n): " BUILD_ARCHIVE
+read -n 1 -p "Should compressed archives (zip, tar.xz) be created? (y/n): " BUILD_ARCHIVE
 
 
 if [ "$BUILD_ARCHIVE" == "y" ]
 then
-tar cvf - "Raspberry Pi Remote Desktop.app" | xz -zkvv9c > "Raspberry Pi Remote Desktop.app.tar.xz"
+#-y is so symbolic links are not followed (they cause a gigantic application and zip file)
+zip -r -9 -y "Raspberry Pi Remote Desktop.app.zip" "Raspberry Pi Remote Desktop.app" &
+tar c "Raspberry Pi Remote Desktop.app" | xz -zkvv9c > "Raspberry Pi Remote Desktop.app.tar.xz"
 fi
 
