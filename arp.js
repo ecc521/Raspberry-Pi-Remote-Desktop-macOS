f30e05de877252864bff74f658d5ee36da48f17b
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-06-09 16:46:46 -0400

    Attempt to handle non-192.168.1.XXX networks.

--- arp.js
+++ arp.js
@@ -3,63 +3,65 @@ const { spawn } = require('child_process');
 
 
 
+
+
 async function getDevices() {
-    
+
     let outputString = ""
-    
+
     await new Promise((resolve, reject) => {
-				
+
 		let detector = spawn("arp", ["-a"], {
 			detached: true,
 			stdio: [ 'ignore', "pipe", "pipe" ]
 		})
-		
+
 		detector.stderr.on("data", reject)
-		
+
 		detector.stdout.on("data", function(data) {
 			outputString += data.toString("utf8")
 		})
-		
+
 		detector.on("close", resolve)
-		
+
 	})
-    
-    
-        
+
+
+
     let outputArray = outputString.split("\n")
-    
+
     outputArray.pop() //remove ending newline
-    
-    
+
+
     for (let i=0;i<outputArray.length;i++) {
         outputArray[i] = outputArray[i].split(" ")
     }
-    
+
     let filteredArray = outputArray.filter((array) => {
-        return !((array[0] === "?" && array[3] === "(incomplete)") || !array[1].slice(1,-1).startsWith("192.168.1."))
+        return !((array[0] === "?" && array[3] === "(incomplete)"))
     })
-    
-    
+
+
     for (let i=0;i<filteredArray.length;i++) {
         let obj = {}
-        
+
         obj.reachable = filteredArray[i][3] !== "(incomplete)"
         obj.mac= filteredArray[i][3]
         obj.ip = filteredArray[i][1].slice(1,-1)
         obj.name = filteredArray[i][0]
-        
+
         filteredArray[i] = obj
-    }    
-    
+    }
+
     filteredArray = filteredArray.sort(function(a,b) {
         if (a.reachable === b.reachable) {return 0}
         if (a.reachable > b.reachable) {return -1}
         return 1
     })
-    
+
     console.log(filteredArray)
     return filteredArray
-    
+
 }
 
 
--- scanner.js
+++ scanner.js
@@ -1,18 +1,37 @@
-const { spawn } = require('child_process');
+const { spawn, spawnSync } = require('child_process');
 
+let output = spawnSync("ifconfig").stdout.toString()
+
+output = output.split(" ")
+let broadcastPosition = output.indexOf("broadcast")
+
+global.thisDevice = output[broadcastPosition - 3]
+global.broadcast = output[broadcastPosition + 1]
+global.broadcast = global.broadcast.slice(0, global.broadcast.indexOf("\n"))
 
 
 
 function createScanners() {
-    //Ping ip's so arp -a picks them up
-    for (let i=0;i<255;i++) {
-        let ip = "192.168.1." + i
-        //Send just 5 packets so this terminates rather quickly
-        let scanner = spawn("ping", ["-c", 5, ip], {
-            detached: true,
-            stdio: [ 'ignore', 'ignore', "ignore" ]
-        })
+
+    //Ideally we ping every possible ip address, because some devices do not reply to broadcast pings.
+    //However if the local network is not 192.168.1.XXX, pinging the broadcast may find us some devices. Plus, pinging the broadcast may allow a quicker startup.
+
+    if (global.broadcast === "192.168.1.255") {
+        //Ping ip's so arp -a picks them up
+        for (let i=0;i<255;i++) {
+            let ip = "192.168.1." + i
+            //Send just 5 packets so this terminates rather quickly
+            let scanner = spawn("ping", ["-c", 5, ip], {
+                detached: true,
+                stdio: [ 'ignore', 'ignore', "ignore" ]
+            })
+        }
     }
+    //Ping broadcast ip so that arp -a picks up the devices that reply
+    let scanner = spawn("ping", ["-c", 5, global.broadcast], {
+        detached: true,
+        stdio: [ 'ignore', 'ignore', "ignore" ]
+    })
 }
 
 
